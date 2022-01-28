ab0f537f1f7d7ecc05f214834eadcabe78afacf4
chrissy-t <86742207+chrissy-t@users.noreply.github.com>
2022-01-27 22:17:56 -0900

    added instructions

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/*Create a simple calculator using an HTML form. 
+The form should have two text inputs to enter the two operands and a radio button to select the operator. 
+Four types of operations should be possible (add, subtract, multiply and divide). 
+The calculator should include an HTML element to provide the result of the operation. 
+There should be submit button that would trigger a calculate function. 
+ Use a loop to access each of the operands as part of a NodeList.
+  Use conditions in your JavaScript to check that the operands are both positive numbers and provide an appropriate message otherwise in the result.
+ Finally, use a switch statement to carry out the appropriate operation based on the ‘value’ of the radio button */
+
 function doSomething() {
     alert('Form submitted!');
     return false;
